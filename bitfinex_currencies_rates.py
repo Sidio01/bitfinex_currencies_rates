796c9d3996e231ccd24e929e6f3d4371839bdadb
Sidio01 <sidio1992@gmail.com>
2021-05-17 19:54:51 +0300

    added posibility to choose file, rewriting existing sheet

--- bitfinex_currencies_rates.py
+++ bitfinex_currencies_rates.py
@@ -1,11 +1,12 @@
-import re
 import datetime
+import re
+from tkinter import filedialog, Tk
 import openpyxl
 import pandas as pd
 import requests
 
 list_of_currencies = []
-list_of_columns = ['SYMBOL', 'BID', 'BID_SIZE', 'ASK', 'ASK_SIZE', 'DAILY_CHANGE', 'DAILY_CHANGE_RELATIVE', 'LAST_PRICE', 'VOLUME', 'HIGH', 'LOW']
+list_of_columns = ['SYMBOL', 'BID', 'BID_SIZE', 'ASK', 'ASK_SIZE', 'DAILY_CHANGE', 'DAILY_CHANGE_RELATIVE', 'LAST_PRICE', 'VOLUME', 'HIGH', 'LOW', 'TIMESTAMP']
 
 url = "https://api-pub.bitfinex.com/v2/tickers?symbols=ALL"
 
@@ -27,9 +28,20 @@ for i, pair in enumerate(response.json(), 1):
         continue
     x = re.split('USD|:USD', pair[0][1:])
     pair[0] = x[0]
+    now = str(datetime.datetime.now())
+    pair.append(now)
     list_of_currencies.append(pair)
 
 df = pd.DataFrame(list_of_currencies, columns=list_of_columns)
-now = str(datetime.datetime.now())
-now = now.replace(':', '.')
-df.to_excel(f'{now}.xlsx', sheet_name=f'{now}')
+
+Tk().withdraw()
+file_name = filedialog.askopenfile()
+
+with pd.ExcelWriter(file_name.name, mode='a') as f:
+    f.book = openpyxl.load_workbook(file_name.name)
+    try:
+        std = f.book['rates']
+        f.book.remove(std)
+    except KeyError:
+        pass
+    df.to_excel(f, sheet_name='rates')
