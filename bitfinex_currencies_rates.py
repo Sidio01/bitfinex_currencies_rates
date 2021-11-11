339b2a56febc0107e8194ea0fdf1de0dc0db3bf9
Sidio01 <sidio1992@gmail.com>
2021-11-11 21:58:46 +0300

    add usd cbr rate

--- bitfinex_currencies_rates.py
+++ bitfinex_currencies_rates.py
@@ -1,21 +1,37 @@
 import datetime
 import re
 from tkinter import filedialog, Tk
-import openpyxl
+import xml.etree.ElementTree as ET
 import pandas as pd
 import requests
 
 list_of_currencies = []
-list_of_columns = ['SYMBOL', 'BID', 'BID_SIZE', 'ASK', 'ASK_SIZE', 'DAILY_CHANGE', 'DAILY_CHANGE_RELATIVE', 'LAST_PRICE', 'VOLUME', 'HIGH', 'LOW', 'TIMESTAMP']
+list_of_columns = ['SYMBOL', 'BID', 'BID_SIZE', 'ASK', 'ASK_SIZE', 'DAILY_CHANGE',
+                   'DAILY_CHANGE_RELATIVE', 'LAST_PRICE', 'VOLUME', 'HIGH', 'LOW', 'TIMESTAMP']
 
-url = "https://api-pub.bitfinex.com/v2/tickers?symbols=ALL"
+today = datetime.datetime.now()
 
-payload={}
+url_bitfinex = "https://api-pub.bitfinex.com/v2/tickers?symbols=ALL"
+url_cbr = f"http://www.cbr.ru/scripts/XML_daily.asp?date_req={today.day}/{today.month}/{today.year}"
+
+payload = {}
 headers = {}
 
-response = requests.request("GET", url, headers=headers, data=payload)
+response_bitfinex = requests.request(
+    "GET", url_bitfinex, headers=headers, data=payload)
+response_cbr = requests.request("GET", url_cbr, headers=headers, data=payload)
 
-for i, pair in enumerate(response.json(), 1):
+root = ET.fromstring(response_cbr.content)
+
+for child in root:
+    if child.attrib["ID"] == "R01235":
+        for c in child:
+            if c.tag == "Value":
+                usd_rate = ["USD", "-", "-", c.text, "-", "-", "-", "-",
+                            "-", "-", "-", f"{today.year}-{today.month}-{today.day}"]
+                list_of_currencies.append(usd_rate)
+
+for i, pair in enumerate(response_bitfinex.json(), 1):
     if len(pair) > 11:
         continue
     if pair[0][-3:] in ['BTC', 'ETH', 'UST', 'XCH', 'EOS', 'EUR', 'JPY', 'GBP', 'BBB']:
@@ -37,11 +53,5 @@ df = pd.DataFrame(list_of_currencies, columns=list_of_columns)
 Tk().withdraw()
 file_name = filedialog.askopenfile()
 
-with pd.ExcelWriter(file_name.name, mode='a') as f:
-    f.book = openpyxl.load_workbook(file_name.name)
-    try:
-        std = f.book['rates']
-        f.book.remove(std)
-    except KeyError:
-        pass
-    df.to_excel(f, sheet_name='rates')
+with pd.ExcelWriter(file_name.name, engine="openpyxl", mode="a", if_sheet_exists="replace") as f:
+    df.to_excel(f, sheet_name="rates", index=False)
